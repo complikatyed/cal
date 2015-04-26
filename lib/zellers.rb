8e0103f53d3e85a75fc1080573f4de7e134c75c0
Katye Russell <katye.russell@gmail.com>
2015-04-25 23:53:23 -0500

    First round of zellers tests passing

--- lib/zellers.rb
+++ lib/zellers.rb
@@ -1,6 +1,40 @@
 class Zellers
+  attr_accessor :month, :year
+
+  def initialize(month, year)
+    @month = month
+    @year = year
+  end
+
+  def start
+
+    h = ["Saturday", "Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday"]
+    day = 1
+
+    if @month <= 2
+      mo = @month + 12 + 1
+      yr = @year -1
+    else
+      mo = @month + 1
+      yr = year
+    end
 
 
+    p1 = ((26 * mo)/10)
+    p2 = (day + yr)
+    p3 = (yr/4)
+    p4 = (6 * (yr/100))
+    p5 = (yr/400)
+    p6 = (p1 + p2 + p3 + p4 + p5)
 
+    puts day
+    puts mo
+    puts yr
+   
 
+    first = p6 % 7
+
+    h[first.to_i]
+  end
+   
 end
--- test/test_zellers.rb
+++ test/test_zellers.rb
@@ -1,7 +1,29 @@
 require_relative 'helper'
+require_relative '../lib/month'
 require_relative '../lib/zellers'
 
 class TestZellers <Minitest::Test
+
+  def test_start_day_for_march_2000
+    z = Zellers.new(03, 2000)
+    assert_equal "Wednesday", z.start
+  end
+
+  def test_start_day_for_dec_1995
+    z = Zellers.new(12, 1995)
+    assert_equal "Friday", z.start
+  end
+  
+  def test_start_day_for_jan_1880
+    z = Zellers.new(01, 1880)
+    assert_equal "Thursday", z.start
+  end
+
+  def test_start_day_for_feb_2014
+    z = Zellers.new(02, 2014)
+    assert_equal "Saturday", z.start
+  end
+
 
 
 
