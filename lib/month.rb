b1495e17f601fda77bc1cd0bfe3e06881487ae97
Katye Russell <katye.russell@gmail.com>
2015-04-30 09:29:50 -0500

    Changed constant WIDTH to MONTH_WIDTH

--- lib/month.rb
+++ lib/month.rb
@@ -2,24 +2,53 @@ require_relative '../lib/zellers'
 
 class Month
   attr_reader :month, :year
-  WIDTH = 20
+  MONTH_WIDTH = 20
 
   def initialize(month, year)
     @month = month.to_i
     @year = year.to_i
   end
 
-  def z
-    z = Zellers.new(month, year)
+  def input_error_message
+    <<EOS
+Date not in acceptable format/range
+`./cal.rb [MM] [YYYY]
+EOS
   end
+
+
 
   def name
-    names = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"]
-    names.at(@month -1)
+    names = ["January",
+             "February",
+             "March",
+             "April",
+             "May",
+             "June",
+             "July",
+             "August",
+             "September",
+             "October",
+             "November",
+             "December"]
+    names.at(@month - 1)
   end
 
+
+  def leap
+
+    if @year % 100 === 0 && @year % 400 === 0
+      leap = true
+    elsif @year % 4 === 0 && @year % 100 != 0
+      leap = true
+    else
+      leap = false
+    end
+  end
+
+
   def date_header
-    "#{name} #{year}".center(WIDTH).rstrip.concat("\n")
+    "#{name} #{year}".center(MONTH_WIDTH).rstrip.concat("\n")
   end
 
   def week_header
@@ -28,23 +57,21 @@ class Month
 
   def month_length
     thirty_days = [9, 4, 6, 11]
-    thirty_one_days = [1, 3, 5, 7, 8, 10, 12]
 
-    if month == 2 and leap?
+    if month == 2 and leap
       29
-    else
+    elsif month == 2 and !leap
       28
-    end
-
-    if thirty_days.include? month
+    elsif thirty_days.include? month
       30
     else
       31
     end
   end
 
-
   def to_s
+
+    z = Zellers.new(month, year)
 
     output = ""
     output << date_header
