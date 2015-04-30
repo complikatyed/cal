8d25ce8799b3811ed92b6918a4cfa404c7ff7d39
Katye Russell <katye.russell@gmail.com>
2015-04-29 22:32:27 -0500

    Added ARGV conditionals.

--- cal.rb
+++ cal.rb
@@ -6,8 +6,25 @@ require_relative 'lib/zellers'
 month = ARGV[0]
 year = ARGV[1]
 
+  def error_message
+    <<EOS
+Date not in acceptable format/range
+`./cal.rb [MM] [YYYY]`
+
+EOS
+  end
+
 
 m = Month.new(month, year)
-z = Zellers.new(month, year)
-puts m.to_s
+
+
+if ARGV.empty? || year.to_i < 1800 || year.to_i > 3000
+  puts error_message
+else
+  puts m.to_s
+end
+
+
+
+
 
