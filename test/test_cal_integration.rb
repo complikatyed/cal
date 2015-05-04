a218c87f7458d68adb000d01bfd50d90d093d064
Katye Russell <katye.russell@gmail.com>
2015-05-04 03:09:33 -0500

    Added full calendar integration test.  All integration tests now passing with year.

--- test/test_cal_integration.rb
+++ test/test_cal_integration.rb
@@ -47,8 +47,8 @@ Su Mo Tu We Th Fr Sa
  8  9 10 11 12 13 14
 15 16 17 18 19 20 21
 22 23 24 25 26 27 28
-29 30 31
-
+29 30 31            
+                    
 EOS
     assert_equal expected, output
   end
@@ -64,7 +64,7 @@ Su Mo Tu We Th Fr Sa
 11 12 13 14 15 16 17
 18 19 20 21 22 23 24
 25 26 27 28 29 30 31
-
+                    
 EOS
     assert_equal expected, output
   end
@@ -79,8 +79,8 @@ Su Mo Tu We Th Fr Sa
  7  8  9 10 11 12 13
 14 15 16 17 18 19 20
 21 22 23 24 25 26 27
-28
-
+28                  
+                    
 EOS
     assert_equal expected, output
   end
@@ -96,7 +96,7 @@ Su Mo Tu We Th Fr Sa
 11 12 13 14 15 16 17
 18 19 20 21 22 23 24
 25 26 27 28 29 30 31
-
+                    
 EOS
     assert_equal expected, output
   end
@@ -111,8 +111,8 @@ Su Mo Tu We Th Fr Sa
  7  8  9 10 11 12 13
 14 15 16 17 18 19 20
 21 22 23 24 25 26 27
-28 29 30
-
+28 29 30            
+                    
 EOS
     assert_equal expected, output
   end
@@ -128,7 +128,7 @@ Su Mo Tu We Th Fr Sa
 11 12 13 14 15 16 17
 18 19 20 21 22 23 24
 25 26 27 28 29 30 31
-
+                    
 EOS
     assert_equal expected, output
   end
@@ -145,7 +145,7 @@ Su Mo Tu We Th Fr Sa
  9 10 11 12 13 14 15
 16 17 18 19 20 21 22
 23 24 25 26 27 28 29
-30
+30                  
 EOS
     assert_equal expected, output
   end
@@ -160,8 +160,8 @@ Su Mo Tu We Th Fr Sa
  6  7  8  9 10 11 12
 13 14 15 16 17 18 19
 20 21 22 23 24 25 26
-27 28 29 30 31
-
+27 28 29 30 31      
+                    
 EOS
     assert_equal expected, output
   end
@@ -176,8 +176,8 @@ Su Mo Tu We Th Fr Sa
  7  8  9 10 11 12 13
 14 15 16 17 18 19 20
 21 22 23 24 25 26 27
-28 29 30 31
-
+28 29 30 31         
+                    
 EOS
     assert_equal expected, output
   end
@@ -192,8 +192,8 @@ Su Mo Tu We Th Fr Sa
  4  5  6  7  8  9 10
 11 12 13 14 15 16 17
 18 19 20 21 22 23 24
-25 26 27 28 29 30
-
+25 26 27 28 29 30   
+                    
 EOS
     assert_equal expected, output
   end
@@ -208,8 +208,8 @@ Su Mo Tu We Th Fr Sa
  6  7  8  9 10 11 12
 13 14 15 16 17 18 19
 20 21 22 23 24 25 26
-27 28 29 30 31
-
+27 28 29 30 31      
+                    
 EOS
     assert_equal expected, output
   end
@@ -225,7 +225,7 @@ Su Mo Tu We Th Fr Sa
  9 10 11 12 13 14 15
 16 17 18 19 20 21 22
 23 24 25 26 27 28 29
-30
+30                  
 EOS
     assert_equal expected, output
   end
@@ -240,8 +240,8 @@ Su Mo Tu We Th Fr Sa
  7  8  9 10 11 12 13
 14 15 16 17 18 19 20
 21 22 23 24 25 26 27
-28 29 30 31
-
+28 29 30 31         
+                    
 EOS
     assert_equal expected, output
   end
@@ -256,8 +256,8 @@ Su Mo Tu We Th Fr Sa
  7  8  9 10 11 12 13
 14 15 16 17 18 19 20
 21 22 23 24 25 26 27
-28 29 30 31
-
+28 29 30 31         
+                    
 EOS
     assert_equal expected, output
   end
@@ -272,8 +272,8 @@ Su Mo Tu We Th Fr Sa
  5  6  7  8  9 10 11
 12 13 14 15 16 17 18
 19 20 21 22 23 24 25
-26 27 28 29 30 31
-
+26 27 28 29 30 31   
+                    
 EOS
     assert_equal expected, output
   end
@@ -287,8 +287,8 @@ Su Mo Tu We Th Fr Sa
  7  8  9 10 11 12 13
 14 15 16 17 18 19 20
 21 22 23 24 25 26 27
-28 29
-
+28 29               
+                    
 EOS
    assert_equal expected, output
   end
@@ -303,8 +303,8 @@ Su Mo Tu We Th Fr Sa
  6  7  8  9 10 11 12
 13 14 15 16 17 18 19
 20 21 22 23 24 25 26
-27 28 29
-
+27 28 29            
+                    
 EOS
     assert_equal expected, output
   end
@@ -318,10 +318,51 @@ Su Mo Tu We Th Fr Sa
  4  5  6  7  8  9 10
 11 12 13 14 15 16 17
 18 19 20 21 22 23 24
-25 26 27 28
-
+25 26 27 28         
+                    
 EOS
     assert_equal expected, output
+  end
+
+  def test_year_prints_all_months_2009
+    y = Year.new(2009)
+    expected = <<EOS
+                             2009
+
+      January               February               March
+Su Mo Tu We Th Fr Sa  Su Mo Tu We Th Fr Sa  Su Mo Tu We Th Fr Sa
+             1  2  3   1  2  3  4  5  6  7   1  2  3  4  5  6  7
+ 4  5  6  7  8  9 10   8  9 10 11 12 13 14   8  9 10 11 12 13 14
+11 12 13 14 15 16 17  15 16 17 18 19 20 21  15 16 17 18 19 20 21
+18 19 20 21 22 23 24  22 23 24 25 26 27 28  22 23 24 25 26 27 28
+25 26 27 28 29 30 31                        29 30 31            
+                                                                
+       April                  May                   June
+Su Mo Tu We Th Fr Sa  Su Mo Tu We Th Fr Sa  Su Mo Tu We Th Fr Sa
+          1  2  3  4                  1  2      1  2  3  4  5  6
+ 5  6  7  8  9 10 11   3  4  5  6  7  8  9   7  8  9 10 11 12 13
+12 13 14 15 16 17 18  10 11 12 13 14 15 16  14 15 16 17 18 19 20
+19 20 21 22 23 24 25  17 18 19 20 21 22 23  21 22 23 24 25 26 27
+26 27 28 29 30        24 25 26 27 28 29 30  28 29 30            
+                      31                                        
+        July                 August              September
+Su Mo Tu We Th Fr Sa  Su Mo Tu We Th Fr Sa  Su Mo Tu We Th Fr Sa
+          1  2  3  4                     1         1  2  3  4  5
+ 5  6  7  8  9 10 11   2  3  4  5  6  7  8   6  7  8  9 10 11 12
+12 13 14 15 16 17 18   9 10 11 12 13 14 15  13 14 15 16 17 18 19
+19 20 21 22 23 24 25  16 17 18 19 20 21 22  20 21 22 23 24 25 26
+26 27 28 29 30 31     23 24 25 26 27 28 29  27 28 29 30         
+                      30 31                                     
+      October               November              December
+Su Mo Tu We Th Fr Sa  Su Mo Tu We Th Fr Sa  Su Mo Tu We Th Fr Sa
+             1  2  3   1  2  3  4  5  6  7         1  2  3  4  5
+ 4  5  6  7  8  9 10   8  9 10 11 12 13 14   6  7  8  9 10 11 12
+11 12 13 14 15 16 17  15 16 17 18 19 20 21  13 14 15 16 17 18 19
+18 19 20 21 22 23 24  22 23 24 25 26 27 28  20 21 22 23 24 25 26
+25 26 27 28 29 30 31  29 30                 27 28 29 30 31      
+                                                                
+EOS
+    assert_equal expected,y.to_s
   end
 
 
