356af823df03e5552812658c8ecc7cdc6ac7e26e
Katye Russell <katye.russell@gmail.com>
2015-05-04 03:06:24 -0500

    Refactored end of line approach to add spaces instead (to accomodate y.to_s).

--- test/test_month.rb
+++ test/test_month.rb
@@ -202,6 +202,21 @@ EOS
   end
 
 
+  def test_to_s_feb_1801
+    m = Month.new(02, 1801)
+    expected = <<EOS
+   February 1801
+Su Mo Tu We Th Fr Sa
+ 1  2  3  4  5  6  7
+ 8  9 10 11 12 13 14
+15 16 17 18 19 20 21
+22 23 24 25 26 27 28
+                    
+                    
+EOS
+    assert_equal expected, m.to_s
+  end
+
 
 
 
