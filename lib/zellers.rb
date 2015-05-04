143bba6b35a15edf36d2da644d1d74cc7f61095a
Katye Russell <katye.russell@gmail.com>
2015-05-04 03:09:53 -0500

    Corrected typo.

--- lib/zellers.rb
+++ lib/zellers.rb
@@ -8,35 +8,34 @@ class Zellers
     @month = month
     @year = year
   end
- 
+
   def start
 
-     day = 1
+    day = 1
 
     if @month <= 2
-      mo = @month + 12 + 1
-      yr = @year -1
+      mo = @month + 12 + day
+      yr = @year - 1
     else
-      mo = @month + 1
+      mo = @month + day
       yr = year
     end
-
-    # Numbers in this section refer to Zeller's convergence
 
     p1 = ((26 * mo)/10)
     p2 = (day + yr)
     p3 = (yr/4)
     p4 = (6 * (yr/100))
     p5 = (yr/400)
+
     p6 = (p1 + p2 + p3 + p4 + p5)
 
-    first = p6 % 7
+    first_day = p6 % 7
 
-    if first == 0
-      first = 7
+    if first_day == 0
+      first_day = 7
     end
 
-    first = first
+    first_day
   end
 
 end
